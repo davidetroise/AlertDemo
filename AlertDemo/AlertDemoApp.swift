d9d87410329161551650365738fec737e696ebe8
Davide Troise <davide@troise.net>
2022-09-08 18:56:02 +0200

    Initial Commit

---
+++ AlertDemo/AlertDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  AlertDemoApp.swift
+//  AlertDemo
+//
+//  Created by Davide Troise with Swift 5.0
+//  for the YouTube channel "Coding con Davide" https://bit.ly/3QJziJE
+//
+
+import SwiftUI
+
+@main
+struct AlertDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
